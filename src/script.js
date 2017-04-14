a633587fc526f2bda751aba78e80a965de9dadfd
David <kansas@inkwhiskey.com>
2017-04-13 23:55:54 -0600

    sloppy but functional nav and added project and testimonials pages

--- src/script.js
+++ src/script.js
@@ -2,7 +2,15 @@
 
 	$(document).ready(function(){
 	  $(".nav-mobile .button").click(function(){
-	    $(".nav").toggle();
+	    $(".nav .nav-item").toggle();
+	    $(".nav-mobile-icon-menu").toggle();
+	    $(".nav-mobile-icon-close").toggle();
+	    $(".nav-logo").toggle();
+	    $(".nav-button-home").toggle();
+	    if($('.nav .inner li div').hasClass('nav-button-home')){
+		  	$('.nav .inner li div').removeClass('nav-button-home')
+			} else if ($('.nav .inner li div').addClass('nav-button-home'))
+	    console.log("menu");
 	  });
 	});
 
