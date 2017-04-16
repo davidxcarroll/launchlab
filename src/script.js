915ac41740491bb85ebf14123c40c3ac816e84fc
David <kansas@inkwhiskey.com>
2017-04-15 23:47:10 -0600

    added list pages for whitepapers and projects and added real images and some style tweaks

--- src/script.js
+++ src/script.js
@@ -6,7 +6,7 @@
 	    $(".nav-mobile-icon-menu").toggle();
 	    $(".nav-mobile-icon-close").toggle();
 	    $(".nav-logo").toggle();
-	    $(".nav-button-home").toggle();
+	    // $(".nav-button-home").toggle();
 	    if($('.nav .inner li div').hasClass('nav-button-home')){
 		  	$('.nav .inner li div').removeClass('nav-button-home')
 			} else if ($('.nav .inner li div').addClass('nav-button-home'))
