dd0fe73173fad70fdc024e93d51d67f507faf4dc
David <kansas@inkwhiskey.com>
2017-04-22 14:19:01 -0600

    updated some photos and other minor tweaks

--- src/_js/script.js
+++ src/_js/script.js
@@ -1,9 +1,9 @@
 <script type="text/javascript">
 
-  $(".nav-mobile .button").click(function(){
+  $(".nav-mobile-button .button").click(function(){
     $(".nav .nav-item").toggle();
-    $(".nav-mobile-icon-menu").toggle();
-    $(".nav-mobile-icon-close").toggle();
+    $(".nav-mobile-button-icon-menu").toggle();
+    $(".nav-mobile-button-icon-close").toggle();
     $(".nav-logo").toggle();
     if($('.nav .inner li div').hasClass('nav-button-home')){
 	  	$('.nav .inner li div').removeClass('nav-button-home')
